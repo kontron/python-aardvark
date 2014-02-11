c06df0c2d76b7c9217dc2a45dcccf9f14647feb6
Michael Walle <michael.walle@kontron.com>
2014-02-11 13:34:45 +0100

    test: add missing test cases
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- test/test_aardvark.py
+++ test/test_aardvark.py
@@ -2,7 +2,7 @@
 
 import array
 import unittest
-from mock import patch, call
+from mock import patch, call, ANY
 import pyaardvark
 
 @patch('pyaardvark.aardvark.api', autospec=True)
@@ -153,6 +153,166 @@ class AardvarkTest(unittest.TestCase):
         a = self.open(api)
         api.py_aa_target_power.return_value = -1
         self.assertRaises(IOError, a.enable_target_power, (0,))
+
+    def test_i2c_master_write(self, api):
+        a = self.open(api)
+        addr = 0x50
+        data = b'\x01\x02\x03'
+        flags = pyaardvark.I2C_NO_STOP | pyaardvark.I2C_SIZED_READ
+        api.py_aa_i2c_write.return_value = 0
+        a.i2c_master_write(addr, data, flags)
+        api.py_aa_i2c_write.assert_called_once_with(
+                a.handle, addr, flags, len(data), array.array('B', data))
+
+    def test_i2c_master_write_default_flags(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_write.return_value = 0
+        a.i2c_master_write(0, '')
+        api.py_aa_i2c_write.assert_called_once_with(
+                ANY, ANY, pyaardvark.I2C_NO_FLAGS, ANY, ANY)
+
+    def test_i2c_master_write_error(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_write.return_value = -1
+        self.assertRaises(IOError, a.i2c_master_write, 0, '')
+
+    def test_i2c_master_read(self, api):
+        def i2c_master_read(_handle, _addr, _flags, length, data):
+            self.assertEqual(data, array.array('B', (0,) * length))
+            for i in range(length):
+                data[i] = i
+            return length
+
+        a = self.open(api)
+        addr = 0x50
+        flags = pyaardvark.I2C_NO_STOP | pyaardvark.I2C_SIZED_READ
+        api.py_aa_i2c_read.side_effect = i2c_master_read
+        data = a.i2c_master_read(addr, 3, flags)
+        api.py_aa_i2c_read.assert_called_once_with(
+                a.handle, addr, flags, 3, ANY)
+        self.assertEqual(data, b'\x00\x01\x02')
+
+    def test_i2c_master_read_default_flags(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_read.return_value = 1
+        data = a.i2c_master_read(0, 0)
+        api.py_aa_i2c_read.assert_called_once_with(
+                ANY, ANY, pyaardvark.I2C_NO_FLAGS, ANY, ANY)
+
+    def test_i2c_master_read_error(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_read.return_value = -1
+        self.assertRaises(IOError, a.i2c_master_read, 0, 0)
+
+    def test_i2c_master_write_read(self, api):
+        def i2c_master_read(_handle, _addr, _flags, length, data):
+            self.assertEqual(data, array.array('B', (0,) * length))
+            for i in range(length):
+                data[i] = i
+            return length
+
+        a = self.open(api)
+        api.py_aa_i2c_write.return_value = 0
+        api.py_aa_i2c_read.side_effect = i2c_master_read
+        addr = 0x50
+        data = a.i2c_master_write_read(addr, b'\x00\x01\x02', 3)
+        api.py_aa_i2c_write.assert_called_once_with(a.handle, addr,
+                pyaardvark.I2C_NO_STOP, len(data), array.array('B', data))
+        api.py_aa_i2c_read.assert_called_once_with(
+                a.handle, addr, pyaardvark.I2C_NO_FLAGS, 3, ANY)
+        self.assertEqual(data, b'\x00\x01\x02')
+
+    def test_i2c_master_write_read_default_flags(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_read.return_value = 1
+        data = a.i2c_master_read(0, 0)
+        api.py_aa_i2c_read.assert_called_once_with(
+                ANY, ANY, pyaardvark.I2C_NO_FLAGS, ANY, ANY)
+
+    def test_i2c_master_write_read_error_read(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_write.return_value = 1
+        api.py_aa_i2c_read.return_value = -1
+        self.assertRaises(IOError, a.i2c_master_write_read, 0, '', 0)
+
+    def test_i2c_master_write_read_error_write(self, api):
+        a = self.open(api)
+        api.py_aa_i2c_write.return_value = -1
+        api.py_aa_i2c_read.return_value = 1
+        self.assertRaises(IOError, a.i2c_master_write_read, 0, '', 0)
+
+    def test_spi_bitrate(self, api):
+        a = self.open(api)
+        api.py_aa_spi_bitrate.return_value = 0
+        a.spi_bitrate(4711)
+        api.py_aa_spi_bitrate.assert_called_once_with(a.handle, 4711)
+
+    def test_spi_bitrate_error(self, api):
+        a = self.open(api)
+        api.py_aa_spi_bitrate.return_value = -1
+        self.assertRaises(IOError, a.spi_bitrate, (0,))
+
+    def test_spi_configure(self, api):
+        a = self.open(api)
+        api.py_aa_spi_configure.return_value = 0
+        a.spi_configure(1, 2, 3)
+        api.py_aa_spi_configure.assert_called_once_with(a.handle, 1, 2, 3)
+
+    def test_spi_configure_error(self, api):
+        a = self.open(api)
+        api.py_aa_spi_configure.return_value = -1
+        self.assertRaises(IOError, a.spi_configure, 0, 0, 0)
+
+    def test_spi_configure_mode_0(self, api):
+        a = self.open(api)
+        api.py_aa_spi_configure.return_value = 0
+        a.spi_configure_mode(pyaardvark.SPI_MODE_0)
+        api.py_aa_spi_configure.assert_called_once_with(a.handle, 0, 0, 0)
+
+    def test_spi_configure_mode_3(self, api):
+        a = self.open(api)
+        api.py_aa_spi_configure.return_value = 0
+        a.spi_configure_mode(pyaardvark.SPI_MODE_3)
+        api.py_aa_spi_configure.assert_called_once_with(a.handle, 1, 1, 0)
+
+    def test_spi_configure_mode_error(self, api):
+        a = self.open(api)
+        api.py_aa_spi_configure.return_value = -1
+        self.assertRaises(IOError, a.spi_configure_mode, 0)
+
+    def test_spi_configure_mode_unknown_mode(self, api):
+        a = self.open(api)
+        self.assertRaises(RuntimeError, a.spi_configure_mode, 1)
+
+    def test_spi_ss_polarity(self, api):
+        a = self.open(api)
+        api.py_aa_spi_master_ss_polarity.return_value = 0
+        a.spi_ss_polarity(42)
+        api.py_aa_spi_master_ss_polarity.assert_called_once_with(a.handle, 42)
+
+    def test_spi_ss_polarity_error(self, api):
+        a = self.open(api)
+        api.py_aa_spi_master_ss_polarity.return_value = -1
+        self.assertRaises(IOError, a.spi_ss_polarity, 0)
+
+    def test_spi_spi_write(self, api):
+        def spi_write(_handle, len_tx, tx, len_rx, rx):
+            self.assertEqual(len_tx, len_rx)
+            for i, v in enumerate(reversed(tx)):
+                rx[i] = v
+            return len_tx
+        a = self.open(api)
+        api.py_aa_spi_write.side_effect = spi_write
+        data = b'\x01\x02\x03'
+        rx_data = a.spi_write(data)
+        self.assertEqual(rx_data, data[::-1])
+        api.py_aa_spi_write.assert_called_once_with(a.handle, len(data),
+                array.array('B', data), len(data), ANY)
+
+    def test_spi_spi_write_error(self, api):
+        a = self.open(api)
+        api.py_aa_spi_write.return_value = -1
+        self.assertRaises(IOError, a.spi_write, '')
 
 if __name__ == '__main__':
     unittest.main()
