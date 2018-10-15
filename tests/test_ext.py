e961f91a8ee6e5b4ec73fc6c038ee8d7b301abfe
Michael Walle <michael.walle@kontron.com>
2018-10-15 13:39:29 +0200

    tests: check external module version
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ tests/test_ext.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import nose
+import pyaardvark
+from nose.tools import eq_
+
+def test_api_version():
+    eq_(pyaardvark.api_version(), '5.30')
+
+if __name__ == '__main__':
+    nose.main()
