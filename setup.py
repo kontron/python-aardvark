ccd50ce8263d4389fc309e20ee1f058265f4d238
Heiko Thiery <heiko.thiery@gmail.com>
2020-10-31 14:12:56 +0100

    setup.py: add python 3.7/3.8
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(name = name,
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Software Development :: Libraries :: Python Modules',
         ],
         entry_points = {
